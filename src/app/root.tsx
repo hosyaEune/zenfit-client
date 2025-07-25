c6560f3d7fd2ee58f1958ae6c28879dacffa9dd7
hosyaeuw <gad972@mail.ru>
2025-07-26 02:37:18 +0300

    vercel manifest test

--- src/app/root.tsx
+++ src/app/root.tsx
@@ -68,15 +68,9 @@ export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
 }
 
 export const links: Route.LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
   {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    rel: "manifest",
+    href: "/manifest.webmanifest",
   },
 ];
 
